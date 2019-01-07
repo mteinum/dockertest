8429105f3b064b9b642dbcac4caf547b945a417d
Morten Teinum <morten.teinum@gmail.com>
2019-01-08 00:07:58 +0100

    initial version

---
+++ receiver/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Receiver
+{
+    class Program
+    {
+        static SemaphoreSlim semaphore = new SemaphoreSlim(0);
+
+        static async Task Main(string[] args)
+        {
+                Console.CancelKeyPress += CancelKeyPress;
+                AppDomain.CurrentDomain.ProcessExit += ProcessExit;
+
+            var host = new Host();
+
+            Console.Title = host.EndpointName;
+
+            await host.Start();
+            await Console.Out.WriteLineAsync("Press Ctrl+C to exit...");
+
+            // wait until notified that the process should exit
+            await semaphore.WaitAsync();
+
+            await host.Stop();
+        }
+
+        static void CancelKeyPress(object sender, ConsoleCancelEventArgs e)
+        {
+            e.Cancel = true;
+            semaphore.Release();
+        }
+
+        static void ProcessExit(object sender, EventArgs e)
+        {
+            semaphore.Release();
+        }
+    }
+}
